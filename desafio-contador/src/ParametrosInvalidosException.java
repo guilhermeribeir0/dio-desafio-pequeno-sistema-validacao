8fb322fdd8201d1b4c7623de0d19f826dbde6b2a
Guilherme Ribeiro <dev.guilhermeribeiro@gmail.com>
2023-10-18 16:17:57 -0300

    ajusta mensagem da excecao

--- desafio-contador/src/ParametrosInvalidosException.java
+++ desafio-contador/src/ParametrosInvalidosException.java
@@ -1,4 +1,4 @@
 public class ParametrosInvalidosException  extends RuntimeException {
 
-    public static final String PARAMETROS_INVALIDOS = "O Primeiro parametro deve ser menor que o Segundo";
+    public static final String PARAMETROS_INVALIDOS = "O 'Primeiro' parametro deve ser menor que o 'Segundo'";
 }
